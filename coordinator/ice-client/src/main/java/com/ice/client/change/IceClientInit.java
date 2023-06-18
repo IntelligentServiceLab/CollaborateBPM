19c6050a59f51252028ca6d73680e0bc1da43932
zwk-666 <66447459+zwk-666@users.noreply.github.com>
2023-06-18 17:29:12 +0800

    Add files via upload

---
+++ coordinator/ice-client/src/main/java/com/ice/client/change/IceClientInit.java
@@ -0,0 +1,69 @@
+package com.ice.client.change;
+
+import com.alibaba.fastjson.JSON;
+import com.ice.client.config.IceClientProperties;
+import com.ice.client.rmi.IceRmiClientServiceImpl;
+import com.ice.client.utils.AddressUtils;
+import com.ice.common.dto.IceTransferDto;
+import com.ice.common.exception.IceException;
+import com.ice.core.utils.IceExecutor;
+import com.ice.rmi.common.server.IceRmiServerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.rmi.registry.Registry;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zjn
+ */
+@Slf4j
+@Service
+@DependsOn({"iceRmiClientInit", "iceBeanFactory", "iceAddressUtils"})
+public final class IceClientInit implements InitializingBean {
+
+    @Resource
+    private IceClientProperties properties;
+
+    @Resource
+    private Registry iceRegistry;
+
+    /*
+     * to avoid loss update msg in init,make init first
+     */
+    @Override
+    public void afterPropertiesSet() {
+        IceExecutor.setExecutor(new ThreadPoolExecutor(properties.getPool().getCoreSize(), properties.getPool().getMaxSize(),
+                properties.getPool().getKeepAliveSeconds(), TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(properties.getPool().getQueueCapacity()), new ThreadPoolExecutor.CallerRunsPolicy()));
+        log.info("ice client init iceStart");
+        IceRmiServerService remoteServerService;
+        try {
+            remoteServerService = (IceRmiServerService) iceRegistry.lookup("IceRemoteServerService");
+        } catch (Exception e) {
+            throw new IceException("ice client connect server error, maybe server is down app:" + properties.getApp(), e);
+        }
+        try {
+            remoteServerService.register(properties.getApp(), AddressUtils.getHost(), properties.getRmi().getPort());
+        } catch (Exception e) {
+            throw new IceException("ice client register error app:" + properties.getApp(), e);
+        }
+        IceTransferDto dto;
+        try {
+            dto = remoteServerService.getInitConfig(properties.getApp());
+        } catch (Exception e) {
+            throw new IceException("ice init error, maybe server is down app:" + properties.getApp(), e);
+        }
+        if (dto != null) {
+            log.info("ice client init content:{}", JSON.toJSONString(dto));
+            IceUpdate.update(dto);
+            log.info("ice client init iceEnd success");
+            IceRmiClientServiceImpl.initEnd(dto.getVersion());
+        }
+    }
+}
---
+++ coordinator/ice-client/src/main/java/com/ice/client/config/IceClientConfig.java
@@ -0,0 +1,25 @@
+package com.ice.client.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * @author zjn
+ */
+@Configuration
+@EnableConfigurationProperties(IceClientProperties.class)
+public class IceClientConfig {
+
+    @Resource
+    private IceClientProperties properties;
+
+    @Bean
+    public Registry iceServerRegistry() throws Exception {
+        return LocateRegistry.getRegistry(properties.getRmi().getServerHost(), properties.getRmi().getServerPort());
+    }
+}
